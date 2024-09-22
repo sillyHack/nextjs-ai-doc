3127d18fc4871218c83429df587d7e65fd229bc1
SillyD <zlidio98@gmail.com>
2024-09-22 03:29:21 +0200

    Setup the app

---
+++ app/ChatApp.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "../components/ui/button";
+
+const ChatApp = () => {
+	return (
+		<div className="flex flex-col gap-3">
+			<h1 className="text-xl">ChatApp</h1>
+			<Button>Click</Button>
+		</div>
+	);
+};
+
+export default ChatApp;
